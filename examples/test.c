81344c2ac7975a58b0796da8176ca31d5963a0a2
Oblivious <me262thanos@gmail.com>
2021-04-14 22:12:00 +0300

    Rearranged tests into separate functions.

--- examples/test.c
+++ examples/test.c
@@ -9,26 +9,32 @@ void print_chars(char c) {
     printf("char: `%c`\n", c);
 }
 
-int main(void) {
+static void string_interpolation_test(void) {
     printf("TESTING STRING INTERPOLATION\n");
     char *str = "Hello World";
     char *num = "42";
     puts("Num: `", num, "` should be `42` and String: `", str, "` should be `Hello World`", "\n");
     puts("My num is: ", num, " and my string is: ", str, "\n");
+}
 
+static void string_iteration_test(void) {
     printf("TESTING STRING ITERATION\n");
     string *iter = string_new("oblivious");
     printf("Iteration of `o b l i v i o u s`\n");
     string_iterate(iter, (string_lambda)print_chars);
     printf("\n");
     string_free(iter);
+}
 
+static void add_format_test(void) {
     printf("TESTING STRING ADD FORMAT\n");
     string *format_buf = string_new("");
     string_addf(format_buf, "%d plus %d is %d, %s, (%g - %g = %g)", 1, 1, 1+1, "This is nice", 5.3, 1.1, 5.3-1.1);
     printf("%s\n\n", string_get(format_buf));
     string_free(format_buf);
-	
+}
+
+static void string_split_test(void) {
     printf("TESTING STRING SPLIT\n");
     string *str_to_split = string_new("cut me in pieces");
 	string *delimeter_to_use = string_new(" ");
@@ -39,14 +45,18 @@ int main(void) {
     string_free(str_to_split);
     string_free(delimeter_to_use);
     vector_free(list_of_tokens);
+}
 
+static void string_equality_test(void) {
     printf("TESTING STRING EQUALITY\n");
     string *one = string_new("test");
     string *two = string_new("test");
     if(string_equals(one, two)) printf("strings `one(\"test\")` and `two(\"test\")` are equal\n\n");
     string_free(one);
     string_free(two);
+}
 
+static void string_litterbin_test(void) {
     printf("TESTING (LITTERBIN) -> STRING\n");
     string *strValue1 = string_new("test value 1");
     string *strValue2 = string_new("test value 2");
@@ -59,6 +69,15 @@ int main(void) {
     string_free(strValue3);
     string_free(strValue4);
     string_free(strValue5);
+}
+
+int main(void) {
+    string_interpolation_test();
+    string_iteration_test();
+    add_format_test();
+    string_split_test();
+    string_equality_test();
+    string_litterbin_test();
 
     return 0;
 }
