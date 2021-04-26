80fe68781e9c7afe68d2f3c7f4f40ae7c2981674
Oblivious <me262thanos@gmail.com>
2021-04-26 13:32:53 +0300

    Updated the split function to use strtok.

--- src/String/string_split.c
+++ src/String/string_split.c
@@ -1,28 +1,21 @@
 #include "headers/string_split.h"
+#include "headers/string_dup.h"
+#include <string.h> /* strtok */
 
 vector *string_split(string *str, string *delimeter) {
 	size_t i;
 	vector *str_tokens = vector_new();
-	string *token_str = string_new("");
+	string *dup = string_dup(str);
+	char *token_ptr = string_get(dup);
 
 	/* TODO -> IMPROVE SPEED */
 	/* Iterate through the chars constructing a string and
 		reseting the value once we find the delimeter */
-	for(i = 0; (i <= string_length(str)
-	&& string_get_char_at_index(str, i) != '\0'); i++) {
-		if(string_get_char_at_index(str, i) == string_get(delimeter)[0]) {
-			/* We found a character matching the delimeter */
-			vector_add(str_tokens, token_str);
-
-			/* Reset the temp string */
-			token_str = string_new("");
-			continue;
-		}
-
-		string_add_char(token_str, string_get_char_at_index(str, i));
+	token_ptr = strtok(string_get(dup), string_get(delimeter));
+	while(token_ptr != NULL) {
+		vector_add(str_tokens, string_new(token_ptr));
+		token_ptr = strtok(NULL, string_get(delimeter));
 	}
 
-	/* We add the last collected characters */
-	vector_add(str_tokens, token_str);
 	return str_tokens;
 }
